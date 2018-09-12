61526a383477f4fdff418df1b9cf9cca492ac25b
Metin San <metinisan@gmail.com>
2018-09-12 19:55:04 +0200

    finished all but 3)

---
+++ Assignment1/2b.py
@@ -0,0 +1,135 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def H(H_0, a, omega_m, omega_l):
+
+	"""
+	Hubble parameter H.
+	
+	"""
+
+	return H_0 * np.sqrt(omega_m * a**(-3) + omega_l)
+
+
+
+def a_dot(H_0, a, omega_m, omega_l):
+
+	"""
+	Derivative of scale factor a.
+
+	"""
+	return H(H_0, a, omega_m, omega_l)*a
+
+
+
+def dvdt(H_0, a, omega_m, omega_l, u, v):
+
+	"""
+	ddelta/dt, where we sub delta = u for readability.
+
+	"""
+
+	h = H(H_0, a, omega_m, omega_l)			#computing H
+	return ((3/2) * u * h**2) - 2*h*v
+
+
+
+def EulerLoop(n, H_0, a_0, a_1, omega_m, omega_l, u_0):
+
+	"""
+	Integrationloop using Eulers Method.
+
+	"""
+
+	#making intial arrays and setting bonudary conditions
+	a, da = np.linspace(a_0, a_1, n, retstep = True)
+	z = (a_1/a) - 1
+	u = np.zeros(n);				u[0] = u_0		
+	v = np.zeros(n);				v[0] = a_dot(H_0, a[0], omega_m, omega_l)
+
+
+	#integration
+	for i in range(n-1):
+		#computing dt
+		dt = da/a_dot(H_0, a[i], omega_m, omega_l)
+
+		v[i+1] = v[i] + dvdt(H_0, a[i], omega_m, omega_l, u[i], v[i])*dt
+		u[i+1] = u[i] + v[i+1]*dt
+
+	return a, u, z
+
+
+
+def GrowthFactor(u, a):
+
+	"""
+	Computing the growth factor f
+
+	"""
+
+	return np.diff(np.log(u))/np.diff(np.log(a))
+
+
+
+def main():
+
+	"""
+	Executes the program and displays the results.
+
+	"""
+
+	#constants and initial values
+	H_0 = 2.269e-18
+	u_0 = 1e-3
+	a_0 = 1e-3
+	a_1 = 1
+	n = int(1e5)
+
+	#fractional densities
+	omega_list = np.array([1.0, 0.3, 0.8])
+
+	#extracting the results
+	for omega_m in omega_list:
+
+		#starting by finding lambda fractional density
+		omega_l = 1 - omega_m
+
+		#computing a and u for the given densities
+		a, u, z = EulerLoop(n, H_0, a_0, a_1, omega_m, omega_l, u_0)
+
+		#plotting the results
+		plt.loglog(a, u, label = r"$\Omega_m$ = %g , $\Omega_\Lambda$ = %g" %(omega_m, omega_l))
+
+
+	plt.title("Time evolution of perturbations")
+	plt.xlabel("$a$")
+	plt.ylabel(r"$\delta$")
+	plt.legend()
+	plt.grid(linestyle="--")
+	plt.savefig("2b.pdf")
+	plt.show()
+
+	#extracting the results
+	for omega_m in omega_list:
+
+		#starting by finding lambda fractional density
+		omega_l = 1 - omega_m
+
+		#computing a and u for the given densities
+		a, u, z = EulerLoop(n, H_0, a_0, a_1, omega_m, omega_l, u_0)
+		#computing the growth factor
+		gf = GrowthFactor(u, a)
+
+		#plotting the results
+		plt.loglog(z[1:], gf , label = r"$\Omega_m$ = %g , $\Omega_\Lambda$ = %g" %(omega_m, omega_l))
+
+	plt.title("Growth Factor for different cosmologies")
+	plt.xlabel("$z$")
+	plt.ylabel("$f$")
+	plt.legend()
+	plt.grid(linestyle="--")
+	plt.savefig("growth.pdf")
+	plt.show()
+
+
+main()
--- Assignment1/Exercise2.py
+++ Assignment1/Exercise2.py
@@ -12,38 +12,49 @@ def f(u, v, t):
 omega_m = np.array([1.0, 0.3, 0.8])
 omega_l = np.array([0.0, 0.7, 0.2])
 
+t00 = (1/2*H_0) * (omega_m[2]/((1 - omega_m[2])**(2/3))) * ( (2/omega_m[2]) \
+	* np.sqrt(1- omega_m[2]) - (1/np.cosh( (2/omega_m[2]) -1 )))
+
 
 #integration definitions
 n = 100000										#integration points
 t_stop = 1/H_0									#end of time
-t_start = t_stop*np.exp(-9/2)					#where our tim begins
+t_start = t_stop*10**(-9/2)					#where our time begins
 dt = (t_stop-t_start)/n 						#steplength
 
 #defining arrays
 t = np.zeros(n)									#time array
 u = np.zeros(n)									#u = delta
 v = np.zeros(n)									#v = u' = delta'
+a = np.zeros(n)
 
 
 #initial conditions
 t[0] = t_start 									#intial time
-u[0] = np.exp(-3)								#initial perturbation delta
+u[0] = 10**(-3)								#initial perturbation delta
 v[0] = (2/3)*t_stop**(-2/3)*t_start**(-1/3)		#initial change in perturbation 
+a[0] = 10**(-3)
+
+
+# print(t[0], t00)
 
 #forward euler method for integration
 for i in range(0,n-1):
 	t[i+1] = t[i] + dt 
+	a[i+1] = (t[i+1]/t_stop)**(2/3)
 	u[i+1] = u[i] + v[i]*dt
 	v[i+1] = v[i] + f(u[i],v[i],t[i])*dt
 
 
+# print(t_stop, t[-1])
 #plotting
-plt.plot(t/t_stop,u, color = "royalblue", label = r"$\delta(t)$")
+# plt.loglog(t/t_stop,u, color = "royalblue", label = r"$\delta(t)$")
+plt.loglog(a,u, color = "royalblue", label = r"$\delta(t)$")
 plt.title("Time evolution of perturbations in EdS")
-plt.xlabel("$t/t_0$")
+plt.xlabel("$a$")
 plt.ylabel(r"$\delta$")
 plt.legend()
 plt.grid(linestyle="--")
 plt.show()
 
-print(u[-1])
+
